963d3536dd05eef0ec1f218d60e012f0da927944
Wahib Farhat <wahibfarhat13@gmail.com>
2022-12-17 15:06:58 +0000

    fix: error when updating parent categories
    fix: Tag API schema

--- blogs/api/serializers.py
+++ blogs/api/serializers.py
@@ -1,10 +1,11 @@
 from mptt.exceptions import InvalidMove
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
+from rest_framework import status
 from django.apps import apps
 from django.conf import settings
+from rest_framework.response import Response
 
-from blogs.models import Category, Post, PostImage, Author
+from blogs.models import Category, Post, PostImage, Author, Tag
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -25,19 +26,34 @@ class CategorySerializer(serializers.ModelSerializer):
             "icon",
         )
 
-    def create(self, validated_data):
-        parent = validated_data.pop("parent")
-        category = super().create(validated_data)
+    @classmethod
+    def create_or_update_parent(cls, instance, parent_uuid):
         try:
-            if parent and parent.get("name"):
-                parent_serializer = CategorySerializer(data={"name": parent.get("name")})
-                parent_serializer.is_valid(raise_exception=True)
-                parent = parent_serializer.save()
-                category.parent = parent
-                category.save()
-            return category
+            if parent_uuid:
+                parent = cls.Meta.model.objects.get(uuid=parent_uuid.get("uuid"))
+                instance.parent = parent
+                instance.save()
+            return instance
+        except cls.Meta.model.DoesNotExist:
+            return Response(
+                status=status.HTTP_404_NOT_FOUND,
+                data={"detail": "parent category does not exist"}
+            )
         except InvalidMove:
-            raise APIException(detail="category may not be a child of itself")
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"detail": "category may not be a child of itself"}
+            )
+
+    def create(self, validated_data):
+        parent_uuid = validated_data.pop("parent")
+        category = super().create(validated_data)
+        return self.__class__.create_or_update_parent(category, parent_uuid)
+
+    def update(self, instance, validated_data):
+        parent_uuid = validated_data.pop("parent")
+        category = super().update(instance, validated_data)
+        return self.__class__.create_or_update_parent(category, parent_uuid)
 
 
 class PostCategorySerializer(serializers.ModelSerializer):
@@ -95,7 +111,7 @@ class PostSerializer(serializers.ModelSerializer):
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Post
+        model = Tag
         exclude = ("id",)
 
 
